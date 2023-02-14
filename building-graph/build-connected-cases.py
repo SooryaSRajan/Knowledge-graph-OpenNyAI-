8d0b644bcec8cfc9361065290256ec785a8530aa
sooryasrajan <soorya.s27@gmail.com>
2023-02-14 15:42:12 +0530

    Changed connection string to connect to remote database

--- building-graph/build-connected-cases.py
+++ building-graph/build-connected-cases.py
@@ -3,10 +3,10 @@ from neo4j import GraphDatabase
 import pandas as pd
 import networkx as nx
 
-with open('../building-dataset/json/processed_data_mvd.json', 'r') as file:
+with open('building-dataset/json/processed_data_mvd.json', 'r') as file:
     data = json.load(file)
 
-driver = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "password"))
+driver = GraphDatabase.driver(uri="neo4j+s://5bdc6a9f.databases.neo4j.io", auth=("neo4j", "jCgUToP_2Qe7UWfqDi8iGj4JCp6k_5I22MZQvflVUVA"))
 
 with driver.session() as session:
     session.run("MATCH (n) DETACH DELETE n")
@@ -43,12 +43,13 @@ for name, group in grouped:
     count = count + 1
 
     for index, row in group.iterrows():
-        G.add_node(row['normalized_name'], type=row['labels'])
+        normalizedName = row['normalized_name'].upper()
+        G.add_node(normalizedName, type=row['labels'])
 
     with driver.session() as session:
         caseName = group.iloc[0]['title']
         caseId = group.iloc[0]['document_id']
-        session.run("CREATE (n:Node {name: $name, type: $type, case_id: $case_id})", name=caseName, type='CASE',
+        session.run("CREATE (n:Case {name: $name, type: $type, case_id: $case_id})", name=caseName, type='CASE',
                     case_id=caseId)
 
         for node in G.nodes:
@@ -61,7 +62,7 @@ for name, group in grouped:
             if relationship is None:
                 relationship = "IS IN"
 
-            session.run("MATCH (a:Node),(b:Node) WHERE a.name = $from_node AND b.name = $to_node AND b.case_id = "
+            session.run("MATCH (a:Node),(b:Case) WHERE a.name = $from_node AND b.name = $to_node AND b.case_id = "
                         "$case_id AND b.type = 'CASE' "
                         " CREATE (a)-[ "
                         "r:RELATIONSHIP {relationship: $relationship}]->(b)", from_node=node, to_node=caseName,
@@ -72,3 +73,5 @@ for name, group in grouped:
         session.run("MATCH (n:Node) WITH n.name AS name, COLLECT(n) AS nodes, COUNT(*) AS count "
                     "WHERE count > 1 "
                     "FOREACH (n IN tail(nodes) | DETACH DELETE n)")
+
+#jCgUToP_2Qe7UWfqDi8iGj4JCp6k_5I22MZQvflVUVA - database password
